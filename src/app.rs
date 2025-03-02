1910775826bdfdfed6d508801a1bc4103d0dfdef
martin-anderson-graham <martin.anderson.graham@gmail.com>
2025-03-02 17:17:19 -0600

    fix font

--- src/app.rs
+++ src/app.rs
@@ -20,13 +20,13 @@ pub fn App() -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/martingraham-blog.css"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@100..800&display=swap" rel="stylesheet" />
-         // favicon
-         <link
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=SUSE:wght@100..800&display=swap" rel="stylesheet"/>
+        // favicon
+        <link
            rel="icon"
            type="image/svg+xml"
            href="data:image/svg+xml,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20width%3D%2764%27%20height%3D%2764%27%3E%3Crect%20width%3D%2764%27%20height%3D%2764%27%20fill%3D%27%23444%27%2F%3E%3Ctext%20x%3D%2750%25%27%20y%3D%2750%25%27%20font-size%3D%2730%27%20fill%3D%27%23fff%27%20text-anchor%3D%27middle%27%20dominant-baseline%3D%27middle%27%3EMG%3C%2Ftext%3E%3C%2Fsvg%3E"
-         />
+        />
 
         // sets the document title
         <Title text="martingraham.dev"/>
