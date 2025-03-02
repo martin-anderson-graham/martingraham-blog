e5f963ca67b4d6ed246568c77f2ac67abff8932c
martin-anderson-graham <martin.anderson.graham@gmail.com>
2025-03-02 17:09:52 -0600

    overhaul styling

--- src/components/layout/header.rs
+++ src/components/layout/header.rs
@@ -3,13 +3,15 @@ use leptos::prelude::*;
 #[component]
 pub fn Header() -> impl IntoView {
     view! {
-        <div>
+        <header>
             <h1>blog.martingraham.dev</h1>
-                <div class="subnav-links">
-                    <a>Posts</a>
-                    <a href="https://github.com/martin-anderson-graham/martingraham-blog" target="_blank">Github</a>
-                    <a href="https://www.martingraham.dev" target="_blank">About me</a>
-                </div>
-            </div>
+                <nav>
+                <ul>
+                    <li><a>Posts</a></li>
+                    <li><a href="https://github.com/martin-anderson-graham/martingraham-blog" target="_blank">Github</a></li>
+                    <li><a href="https://www.martingraham.dev" target="_blank">About me</a></li>
+                    </ul>
+                </nav>
+            </header>
     }
 }
--- src/posts/first.md
+++ src/posts/first.md
@@ -5,3 +5,8 @@ I wanted to blog about trying to implement the actor model for prediction-ledger
 So then I wanted to write my own blog in rust (as an opportunity to use leptos).
 
 So I think I've nerd-sniped myself.
+
+```ts
+const a = "does this work"
+console.log(a)
+```
